b99010cb634e5940299f9e138a362dd0263fe341
NourH2000 <ninamimy43@gmail.com>
2022-12-30 16:07:23 +0100

    UserList added

--- assignment_Front_End/src/features/users/UsersList.jsx
+++ assignment_Front_End/src/features/users/UsersList.jsx
@@ -1,39 +1,85 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectAllUsers, fetchUsers } from "./userSlice";
+import { Spinner } from "../../components/Spinner";
+/*  */
 
 const UsersList = () => {
+  const dispatch = useDispatch();
+  const users = useSelector(selectAllUsers);
+  const usersStatus = useSelector((state) => state.users.status);
+  const usersError = useSelector((state) => state.users.error);
+
+  useEffect(() => {
+    if (usersStatus === "idle") {
+      console.log(users);
+      dispatch(fetchUsers());
+    }
+  }, [usersStatus, dispatch]);
+
+  let content;
+  if (usersStatus === "loading") {
+    content = <Spinner text="Loading..." />;
+  } else if (usersStatus === "succeeded") {
+    content = (
+      <table className="border-collapse border border-slate-300 ">
+        <thead className="bg-gray-300 h-10">
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>UserName</th>
+            <th>Email</th>
+            <th>Gender</th>
+            <th>Delete</th>
+            <th>Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr>
+              <td className="border-b border-slate-200 py-4 text-center">
+                {user.id}
+              </td>
+              <td className="border-b border-slate-200 py-4 text-center">
+                {user.first_name}
+              </td>
+              <td className="border-b border-slate-200 py-4 text-center">
+                {user.last_name}
+              </td>
+              <td className="border-b border-slate-200 py-4 text-center">
+                {user.email}
+              </td>
+              <td className="border-b border-slate-200 py-4 text-center">
+                {user.gender}
+              </td>
+              <td className="border-b border-slate-200 py-4 text-center">
+                <button className="bg-[#eab308] px-5 py-2 text-white font-bold rounded-lg">
+                  Edit
+                </button>
+              </td>
+              <td className="border-b border-slate-200 py-4 text-center">
+                <button className="bg-[#ef4444] px-5 py-2 text-white font-bold rounded-lg">
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  } else if (usersStatus === "failed") {
+    content = <div>{usersError}</div>;
+  }
   return (
-    <div className="h-screen w-full flex justify-center pt-20 ">
-      <div className="flex flex-col  w-[900px] p-5 border rounded">
-        <div className="flex justify-between mb-6">
+    <div className="h-screen w-full flex justify-center pt-20">
+      <div className="flex flex-col  w-[1000px] p-5 border rounded  ">
+        <div className="flex justify-between mb-10">
           <div className="font-bold text-3xl ">Dashbord</div>
           <button className="p-3 bg-blue-600 text-white rounded font-bold">
             Add user
           </button>
         </div>
-        <table class="border-collapse border border-slate-300...">
-          <thead className="bg-gray-300 h-10">
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>User Name</th>
-              <th>Email</th>
-              <th>City</th>
-              <th>Delete</th>
-              <th>Edit</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td class="border-b border-slate-700 ...">Indiana</td>
-              <td class="border-b border-slate-700 ...">Indianapolis</td>
-              <td class="border-b border-slate-700 ...">Indiana</td>
-              <td class="border-b border-slate-700 ...">Indianapolis</td>
-              <td class="border-b border-slate-700 ...">Indiana</td>
-              <td class="border-b border-slate-700 ...">Indianapolis</td>
-              <td class="border-b border-slate-700 ...">Indianapolis</td>
-            </tr>
-          </tbody>
-        </table>
+        {content}
       </div>
     </div>
   );
---
+++ assignment_Front_End/src/features/users/userSlice.jsx
@@ -0,0 +1,53 @@
+import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+// Initial user state
+const initialState = {
+  users: [],
+  status: "idle",
+  error: null,
+};
+
+// Fetch all users :
+
+var configGetUsers = {
+  method: "get",
+  url: "http://localhost:3000/data",
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+    "access-control-allow-origin": "*",
+  },
+};
+
+export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+  const response = await axios(configGetUsers);
+
+  return response.data;
+});
+// User Slice
+const userSlice = createSlice({
+  name: "users",
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchUsers.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.users = state.users.concat(action.payload);
+      })
+      .addCase(fetchUsers.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default userSlice.reducer;
+
+export const selectAllUsers = (state) => state.users.users;
+
+export const selectUsersById = (state, userId) =>
+  state.users.users.find((user) => user.id === userId);
